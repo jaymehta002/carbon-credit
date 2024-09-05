c5af4f33f4126dc968f66ace3349166788cba87c
lakshya-roonwal <lakshyaroonwal@gmail.com>
2024-09-05 22:54:02 +0530

    feat : dashboard routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,12 @@
-"use client"
-import { Sidebar, SidebarBody, SidebarLink } from "@/components/Dashboard/sidebar";
+"use client";
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarLink,
+} from "@/components/Dashboard/sidebar";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import {
-LayoutDashboard ,
-User 
-} from "lucide-react";
+import { LayoutDashboard, User, Wallet, Plus } from "lucide-react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
@@ -14,12 +15,30 @@ const links = [
   {
     label: "Dashboard",
     href: "/dashboard",
-    icon: <LayoutDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    icon: (
+      <LayoutDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Add Project",
+    href: "/dashboard/addproject",
+    icon: (
+      <Plus className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Wallet",
+    href: "/dashboard/wallet",
+    icon: (
+      <Wallet className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
   },
   {
     label: "Profile",
     href: "/dashboard/profile",
-    icon: <User className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    icon: (
+      <User className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
   },
 ];
 
@@ -60,10 +79,12 @@ export default function RootLayout({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className={cn(
+    <div
+      className={cn(
         "rounded-md flex flex-col md:flex-row dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
         "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
-      )}>
+      )}
+    >
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
@@ -75,13 +96,11 @@ export default function RootLayout({
             </div>
           </div>
           <div className="flex items-center justify-between gap-2">
-            <UserButton/>
+            <UserButton />
           </div>
         </SidebarBody>
       </Sidebar>
-      <main className="flex-1 overflow-y-auto">
-        {children}
-      </main>
+      <main className="flex-1 overflow-y-auto">{children}</main>
     </div>
   );
 }
